1d2096649c12b2a6fa06c2a098768a2bed31d8c1
DM <dominik.mrkos@gmail.com>
2020-01-19 20:51:07 +0100

    removed unused packages

--- SeleniumGoogleCalc/src/test/java/com/foo/PageObjectModel.java
+++ SeleniumGoogleCalc/src/test/java/com/foo/PageObjectModel.java
@@ -1,13 +1,9 @@
 package com.foo;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Wait;
-
-import javax.management.remote.rmi._RMIConnection_Stub;
 
 public class PageObjectModel {
 
